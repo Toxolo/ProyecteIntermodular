057ddbda24e841cb6ec75c8c3d58911715743dad
Alumne DAM <alumne.dam.ieseljust@gmail.com>
2026-01-21 12:14:59 +0100

    save videos prueba

--- SpringBoot/src/main/java/org/padalustro/presentation/controller/VideoCatalegController.java
+++ SpringBoot/src/main/java/org/padalustro/presentation/controller/VideoCatalegController.java
@@ -5,13 +5,19 @@ import java.util.List;
 import org.padalustro.application.usecase.Cataleg.DeleteVideoCatalegUseCase;
 import org.padalustro.application.usecase.Cataleg.GetAllVideoCatalegUseCase;
 import org.padalustro.application.usecase.Cataleg.GetVideoCatalegByIdUseCase;
+import org.padalustro.domain.repository.VideoCatalegRepository;
 import org.padalustro.infrastructure.DTO.VideoCatalegDTO;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 
 
 
@@ -19,7 +25,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/Cataleg")
 public class VideoCatalegController {
-
+    @Autowired
+    private VideoCatalegRepository videoCatalegRepository;
     private final GetAllVideoCatalegUseCase getAllVideoCataleg;
     private final GetVideoCatalegByIdUseCase getVideoCatalegById;
     private final DeleteVideoCatalegUseCase deleteVideoCataleg;
@@ -58,4 +65,25 @@ public class VideoCatalegController {
         deleteVideoCataleg.execute(id);
         return ResponseEntity.noContent().build(); // 204
     }
+    @PostMapping
+    public ResponseEntity<Void> createVideo(
+            @RequestBody VideoCatalegDTO video
+    ) {
+        videoCatalegRepository.saveVideoCataleg(video);
+        return ResponseEntity.status(201).build();
+    }
+    @PutMapping("/{id}")
+    public ResponseEntity<Void> updateVideo(
+            @PathVariable Long id,
+            @RequestBody VideoCatalegDTO video
+    ) {
+        if (!videoCatalegRepository.existsById(id)) {
+            return ResponseEntity.notFound().build();
+        }
+
+        videoCatalegRepository.updateVideoCataleg(id, video);
+        return ResponseEntity.ok().build();
+    }
+
+
 }
