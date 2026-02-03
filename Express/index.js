a5b11a908e126cc80b6cf35fd4034f425fa91c47
Alumne DAM <alumne.dam.ieseljust@gmail.com>
2026-02-03 10:09:19 +0100

    ahora es https

--- Express/index.js
+++ Express/index.js
@@ -1,13 +1,25 @@
 import express from 'express';
+import https from 'https';
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 const port = 3000;
+const httpsOptions = { // Carga certificados para tener HTTPS
+  key: fs.readFileSync(path.join(__dirname, 'keys', 'private.key')),
+  cert: fs.readFileSync(path.join(__dirname, 'keys', 'certificate.crt')),
+};
+
 
 import cors from 'cors';
 
 app.use(cors({
   origin: [
-    'http://localhost:1420'
+    'https://localhost:1420'
   ],
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization','X-Client-Id'],
@@ -16,23 +28,17 @@ app.use(cors({
 
 
 // WebSocket setup
-import http from "http";
 import {UploadScreenSocket} from "./src/websockets/uploadScreenSocket.js";
-const server = http.createServer(app);
+const server = https.createServer(httpsOptions, app); // Creamos servidor HTTPS
 const uploadScreenSocket = new UploadScreenSocket();
 uploadScreenSocket.initialize(server);
 app.set('uploadScreenSocket', uploadScreenSocket);
 
 
 import Routes from './src/routes/routes.js';
-import path from 'path';
-import { fileURLToPath } from 'url';
 import {initDb} from './src/controllers/VideoController.js'
 
 await initDb();
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 
 app.use(express.json());
@@ -48,5 +54,5 @@ console.log(publicPath);
 app.use(Routes);
 
 server.listen(port, '0.0.0.0', () => {
-  console.log(`Listening on http://localhost:${port}`);
+  console.log(`Listening on https://localhost:${port}`);
 });
