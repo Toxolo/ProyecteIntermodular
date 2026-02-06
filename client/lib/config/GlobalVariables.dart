2b19d66717f5304c670e147dd59c4a96f2144881
Alumne DAM <alumne.dam.ieseljust@gmail.com>
2026-02-06 09:58:07 +0100

    ns

--- client/lib/config/GlobalVariables.dart
+++ client/lib/config/GlobalVariables.dart
@@ -1,4 +1,4 @@
-const ip = '192.168.1.149';
+const ip = '10.0.2.2';
 
 const String baseUrl = 'http://$ip:8090';
 
@@ -8,8 +8,3 @@ const String getTokenUrl = 'http://$ip:8069/api/authenticate';
 
 const String refreshTokenUrl = 'http://$ip:8069/api/update/refresh-token';
 
-const ip = '10.0.2.2';
-
-const String baseUrl = 'http://$ip:8090';
-
-const String getTokenUrl = 'http://$ip:8069/api/authenticate';
